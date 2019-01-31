29e8b58896ad5e1191bd8b131b65a82a5ad2a202
James <jamesneyer@gmail.com>
2019-01-31 17:20:46 -0500

    Fix size variable

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ const child = require('child_process');
 exports.handler = function blockies_generator(event, context, callback) {
     console.log(JSON.stringify(event, null, 2));
     const request = event.Records[0].cf.request;
-    console.log(request)
     
     const targetPath = '/tmp/targetImage';
     
     const options = querystring.parse(request.querystring);
-    const small = [8, 4];
-    const medium = [8, 8];
 
-    const sizeNames = ['small', 'large'];
-    const sizes = [{'small': small, 'medium': medium}];
+    const small = 4;
+    const medium = 8;
+    const large = 16;
+    
+    const sizeNames = ['small', 'medium', 'large'];
+    const sizes = {'small': small, 'medium': medium, 'large': large};
+
+    var size = medium;
     
     if (options.size && !sizeNames.includes(options.size)) {
         console.log('Invalid Input');
@@ -30,25 +33,34 @@ exports.handler = function blockies_generator(event, context, callback) {
             statusDescription: 'Invalid Input'
         });
         return;
+    } else if (options.size && sizeNames.includes(options.size)) {
+        size = sizes[options.size];
     }
 
-    const size = options.size | small;
-    const sizeDim = sizes[size];
     const web3 = new Web3();
 
-    if (options.wallet && (web3.utils.isAddress(options.wallet))){
+    if (request.uri && !request.uri.includes('.png')) {
+        console.log('Invalid ')
+        context.succeed({
+            status: '400',
+            statusDescription: 'Invalid Input'
+        });
+    }
+
+    const wallet = request.uri.substr(1).split('.')[0].toLowerCase();;
+
+    if (request.uri && (web3.utils.isAddress(wallet))){
         const canvas = Canvas.createCanvas(50, 50);
 
         var icon = blockies.render({
-            seed: options.wallet,
-            size: sizeDim[0],
-            scale: sizeDim[1]
+            seed: wallet,
+            scale: size
         }, canvas);
 
         var stringIcon = icon.toDataURL().replace(/^data:image\/(png|jpg);base64,/, '');
 
         context.succeed({
-            bodyEncoding: 'text',
+            bodyEncoding: 'base64',
             body: stringIcon,
             status: '200',
             statusDescription: 'OK',
