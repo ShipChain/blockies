06a6058a6012e35fb05b38bcb587ecf0cffb4e08
James <jamesneyer@gmail.com>
2019-02-04 16:37:27 -0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": false
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "env": {
+        "amd": true
+    },
+};
